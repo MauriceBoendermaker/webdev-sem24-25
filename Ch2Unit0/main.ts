02cbf06402e49369548d85223dd1465f165aa2bc
Maurice <mauriceboendermaker@gmail.com>
2024-11-20 12:19:35 +0100

    Part of Ch2Unit0

---
+++ Ch2Unit0/main.ts
@@ -0,0 +1,47 @@
+console.log("Hello!")
+
+let x = 100
+
+console.log(x)
+{
+    let x = "Hello world!"
+    console.log(x)
+}
+console.log(x)
+
+let i = 2
+let j = 3
+console.log(i + j)
+console.log(i * j)
+console.log(i / j)
+console.log(i % j)
+console.log(i ** j)
+console.log(Math.log2(i ** j))
+
+let ii = 2n
+let jj = 3n
+console.log(ii + jj)
+console.log(ii * jj)
+console.log(ii / jj)
+console.log(ii % jj)
+console.log(ii ** jj)
+
+let iii = true
+let jjj = false
+console.log(iii && jjj)
+console.log(iii || jjj)
+console.log(!iii)
+
+let iiii = "hello"
+let jjjj = "world"
+console.log(iiii + jjjj)
+console.log(`${iiii} ${jjjj}`)
+console.log(iiii.length)
+console.log(`${iiii} ${jjjj}${"!".repeat(5)}`)
+console.log(`${iiii.replace("h", "H")} ${jjjj}${"!".repeat(5)}`)
+
+let xx:string = "hello"
+let yy:number = 2
+console.log(xx + yy)
+
+// Conditionals and onward goes here
