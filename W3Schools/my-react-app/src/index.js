a38e721ae58ec6c8dcf32657589de909f843e979
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2024-12-26 14:23:17 +0100

    React ES6 Arrow Functions

--- W3Schools/my-react-app/src/index.js
+++ W3Schools/my-react-app/src/index.js
@@ -11,6 +11,20 @@ class Car {
     present() {
         return 'I have a ' + this.brand;
     }
+
+    hello = function () {
+        return "Hello World!";
+    }
+
+    hello = () => {
+        return "Hello World!";
+    }
+
+    hello = () => "Hello World!";
+
+    hello = (val) => "Hello " + val;
+
+    hello = val => "Hello " + val;
 }
 
 class Model extends Car {
@@ -25,3 +39,29 @@ class Model extends Car {
 
 const mycar = new Model("Ford", "Mustang");
 root.render(mycar.show());
+
+class Header {
+    constructor() {
+        this.color = "Red";
+    }
+
+    // Regular function:
+    changeColor = function () {
+        document.getElementById("demo").innerHTML += this;
+    }
+
+    // Arrow function:
+    changeColor2 = () => {
+        document.getElementById("demo2").innerHTML += this;
+    }
+}
+
+const myheader = new Header();
+
+// The window object calls the function:
+window.addEventListener("load", myheader.changeColor);
+window.addEventListener("load", myheader.changeColor2);
+
+// A button object calls the function:
+document.getElementById("btn").addEventListener("click", myheader.changeColor);
+document.getElementById("btn2").addEventListener("click", myheader.changeColor2);
