210f138888d77c27356ccd7e310a5f173e6b1e03
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2024-12-27 13:13:25 +0100

    React JSX

--- W3Schools/my-react-app/src/index.js
+++ W3Schools/my-react-app/src/index.js
@@ -27,6 +27,7 @@ class Car {
     hello = val => "Hello " + val;
 }
 
+
 class Model extends Car {
     constructor(name, mod) {
         super(name);
@@ -39,6 +40,7 @@ class Model extends Car {
 
 const mycar = new Model("Ford", "Mustang");
 root.render(mycar.show());
+
 
 class Header {
     constructor() {
@@ -66,15 +68,81 @@ window.addEventListener("load", myheader.changeColor2);
 document.getElementById("btn").addEventListener("click", myheader.changeColor);
 document.getElementById("btn2").addEventListener("click", myheader.changeColor2);
 
+
 var x = 5.6;
 let y = 5.6;
 const z = 5.6;
+
 
 const myArray = ['apple', 'banana', 'orange']
 
 const myList = myArray.map((item) => <p>{item}</p>)
 root.render(myList)
 
+
 const vehicles = ['mustang', 'f-150', 'expedition'];
 
 const [car, , suv] = vehicles;
+
+
+const myJSXElement = <h1>I Love JSX!</h1>;
+const rootJSX = ReactDOM.createRoot(document.getElementById('rootJSX'));
+rootJSX.render(myJSXElement);
+
+const myNotJSXElement = React.createElement('h1', {}, 'I do not use JSX!');
+const rootNotJSX = ReactDOM.createRoot(document.getElementById('rootNotJSX'));
+rootNotJSX.render(myNotJSXElement);
+
+const myExpressionElement = <h1>React is {5 + 5} times better with JSX</h1>;
+const rootJSXExpression = ReactDOM.createRoot(document.getElementById('rootJSXExpression'));
+rootJSXExpression.render(myExpressionElement);
+
+const myLargeHTMLElement = (
+    <ul>
+        <li>Apples</li>
+        <li>Bananas</li>
+        <li>Cherries</li>
+    </ul>
+);
+const rootLargeHTMLElement = ReactDOM.createRoot(document.getElementById('rootLargeHTMLElement'));
+rootLargeHTMLElement.render(myLargeHTMLElement)
+
+const myTopLevelElement = (
+    <div>
+        <p>I am the first paragraph.</p>
+        <p>I am the second paragraph.</p>
+    </div>
+);
+const rootTopLevelElement = ReactDOM.createRoot(document.getElementById('rootTopLevelElement'));
+rootTopLevelElement.render(myTopLevelElement);
+
+const myFragmentElement = (
+    <>
+        <p>I am the first paragraph, wrapped inside of an empty HTML tag.</p>
+        <p>I am the second paragraph, wrapped inside of an empty HTML tag.</p>
+    </>
+);
+const rootFragmentElement = ReactDOM.createRoot(document.getElementById('rootFragmentElement'));
+rootFragmentElement.render(myFragmentElement);
+
+const myClosedElement = <input type="text" />;
+const rootClosedElement = ReactDOM.createRoot(document.getElementById('rootClosedElement'));
+rootClosedElement.render(myClosedElement);
+
+const myClassElement = <h1 className="myClass">Hello World, I have a className as attribute</h1>;
+const rootClassAttribute = ReactDOM.createRoot(document.getElementById('rootClassAttribute'));
+rootClassAttribute.render(myClassElement);
+
+const xIf = 1; // 11
+let text = "Goodbye";
+if (xIf < 10) {
+    text = "Hello";
+}
+const myIfStatementElement = <h1>{text}</h1>
+const rootIfStatementElement = ReactDOM.createRoot(document.getElementById('rootIfStatementElement'));
+rootIfStatementElement.render(myIfStatementElement);
+
+const xTernary = 11; // 1
+const myTernaryOperatorElement = <h1>{(xTernary) < 10 ? "Hello" : "Goodbye"}</h1>;
+const rootTernaryOperatorElement = ReactDOM.createRoot(document.getElementById('rootTernaryOperatorElement'));
+rootTernaryOperatorElement.render(myTernaryOperatorElement);
