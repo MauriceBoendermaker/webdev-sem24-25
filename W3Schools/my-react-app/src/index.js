7e6a4d0e7038d844122a4a8a068ca59cfea14a5f
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2024-12-30 20:18:41 +0100

    React Hooks

--- W3Schools/my-react-app/src/index.js
+++ W3Schools/my-react-app/src/index.js
@@ -518,3 +518,36 @@ const MyMemoApp = () => {
 };
 const rootMyMemoApp = ReactDOM.createRoot(document.getElementById('rootMyMemoApp'));
 rootMyMemoApp.render(<MyMemoApp />);
+
+
+function FavoriteColor() {
+    const [color, setColor] = useState("red");
+
+    return (
+        <>
+            <h1>My favorite color is {color}!</h1>
+            <button
+                type="button"
+                onClick={() => setColor("blue")}>
+                Blue
+            </button>
+            <button
+                type="button"
+                onClick={() => setColor("red")}>
+                Red
+            </button>
+            <button
+                type="button"
+                onClick={() => setColor("pink")}>
+                Pink
+            </button>
+            <button
+                type="button"
+                onClick={() => setColor("green")}>
+                Green
+            </button>
+        </>
+    )
+}
+const rootMyHookExample = ReactDOM.createRoot(document.getElementById('rootMyHookExample'));
+rootMyHookExample.render(<FavoriteColor />)
