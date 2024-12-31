475d7500159a5e3daf7f1b5698203e6202ee5c99
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2024-12-31 12:48:32 +0100

    TypeScript Type Aliases and Interfaces

--- W3Schools/TypeScript/bin/main.js
+++ W3Schools/TypeScript/bin/main.js
@@ -126,4 +126,27 @@ var Enums;
     // logs "West"
     console.log(CardinalDirections_String.West);
 })(Enums || (Enums = {}));
+var TypeAliases_and_Interfaces;
+(function (TypeAliases_and_Interfaces) {
+    const carYear = 2001;
+    const carType = "Toyota";
+    const carModel = "Corolla";
+    const car = {
+        year: carYear,
+        type: carType,
+        model: carModel
+    };
+    console.log(car);
+    const rectangle = {
+        height: 20,
+        width: 10
+    };
+    console.log(rectangle);
+    const coloredRectangle = {
+        height: 20,
+        width: 10,
+        color: "red"
+    };
+    console.log(coloredRectangle);
+})(TypeAliases_and_Interfaces || (TypeAliases_and_Interfaces = {}));
 //# sourceMappingURL=main.js.map
--- W3Schools/TypeScript/main.ts
+++ W3Schools/TypeScript/main.ts
@@ -151,3 +151,48 @@ namespace Enums {
     // logs "West"
     console.log(CardinalDirections_String.West);
 }
+
+namespace TypeAliases_and_Interfaces {
+    type CarYear = number
+    type CarType = string
+    type CarModel = string
+    type Car = {
+        year: CarYear,
+        type: CarType,
+        model: CarModel
+    }
+
+    const carYear: CarYear = 2001
+    const carType: CarType = "Toyota"
+    const carModel: CarModel = "Corolla"
+    const car: Car = {
+        year: carYear,
+        type: carType,
+        model: carModel
+    };
+    console.log(car);
+
+
+    interface Rectangle {
+        height: number,
+        width: number
+    }
+
+    const rectangle: Rectangle = {
+        height: 20,
+        width: 10
+    };
+    console.log(rectangle);
+
+
+    interface ColoredRectangle extends Rectangle {
+        color: string
+    }
+
+    const coloredRectangle: ColoredRectangle = {
+        height: 20,
+        width: 10,
+        color: "red"
+    };
+    console.log(coloredRectangle);
+}
