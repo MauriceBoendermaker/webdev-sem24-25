64dfc1b5725ce738845c6ae1a92fd1a347f240be
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2024-12-31 09:21:00 +0100

    TypeScript Special Types

--- W3Schools/TypeScript/bin/main.js
+++ W3Schools/TypeScript/bin/main.js
@@ -6,6 +6,29 @@ var SimpleTypes;
     console.log(typeof firstNameExplicit);
     console.log(typeof firstNameImplicit);
     let firstNameError = "Dylan";
-    // firstNameError = 33; // gives an error   
+    // firstNameError = 33; // error
 })(SimpleTypes || (SimpleTypes = {}));
+var SpecialTypes;
+(function (SpecialTypes) {
+    let u = true;
+    // u = "string"; // error
+    // Math.round(u); // error
+    let v = true;
+    v = "string"; // no error
+    console.log(Math.round(v)); // no error
+    let w = 1;
+    w = "string"; // no error
+    w = {
+        runANonExistentMethod: () => {
+            console.log("I think therefore I am");
+        }
+    };
+    // w.runANonExistentMethod(); // error
+    if (typeof w === 'object' && w !== null) {
+        w.runANonExistentMethod();
+    }
+    // let x: never = true; // error
+    let y = undefined;
+    let z = null;
+})(SpecialTypes || (SpecialTypes = {}));
 //# sourceMappingURL=main.js.map
--- W3Schools/TypeScript/main.ts
+++ W3Schools/TypeScript/main.ts
@@ -5,6 +5,38 @@ namespace SimpleTypes {
     console.log(typeof firstNameExplicit);
     console.log(typeof firstNameImplicit);
 
+
     let firstNameError: string = "Dylan";
-    // firstNameError = 33; // gives an error   
+    // firstNameError = 33; // error
+}
+
+namespace SpecialTypes {
+    let u = true;
+    // u = "string"; // error
+    // Math.round(u); // error
+
+
+    let v: any = true;
+    v = "string"; // no error
+    console.log(Math.round(v)); // no error
+
+
+    let w: unknown = 1;
+    w = "string"; // no error
+    w = {
+        runANonExistentMethod: () => {
+            console.log("I think therefore I am");
+        }
+    } as { runANonExistentMethod: () => void }
+    // w.runANonExistentMethod(); // error
+    if (typeof w === 'object' && w !== null) {
+        (w as { runANonExistentMethod: Function }).runANonExistentMethod();
+    }
+
+
+    // let x: never = true; // error
+
+
+    let y: undefined = undefined;
+    let z: null = null;
 }
